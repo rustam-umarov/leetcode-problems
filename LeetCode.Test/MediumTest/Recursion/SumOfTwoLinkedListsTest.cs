ec7a5df37c9228679e9dabbca7c61ab5f183901f
Rustam Umarov <rustam.umarov@yahoo.com>
2021-11-11 16:27:12 -0600

    add two linkedlists and return linkedlist: medium

---
+++ LeetCode.Test/MediumTest/Recursion/SumOfTwoLinkedListsTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Medium.Recursion;
+
+namespace LeetCode.Test.MediumTest.Recursion
+{
+    [TestClass]
+    public class SumOfTwoLinkedListsTest
+    {
+        private SumOfTwoLinkedLists _recursiveLinkedListSum;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _recursiveLinkedListSum = new SumOfTwoLinkedLists();
+        }
+
+        [TestMethod]
+        public void GivenNumbersWithEqualLength()
+        {
+            var l1 = new Node
+            {
+                Value = 2,
+                Next = new Node
+                {
+                    Value = 4,
+                    Next = new Node
+                    {
+                        Value = 3,
+                        Next = null
+                    }
+                }
+            };
+
+            var l2 = new Node
+            {
+                Value = 5,
+                Next = new Node
+                {
+                    Value = 6,
+                    Next = new Node
+                    {
+                        Value = 4,
+                        Next = null
+                    }
+                }
+            };
+
+            var result = _recursiveLinkedListSum.Solution(l1, l2);
+
+            int[] answer = { 7, 0, 8 };
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Assert.AreEqual(answer[i], result.Value);
+                result = result.Next;
+            }
+        }
+
+        [TestMethod]
+        public void GivenNumbersWithDifferentLength()
+        {
+            var l1 = new Node
+            {
+                Value = 9,
+                Next = new Node
+                {
+                    Value = 9,
+                    Next = new Node
+                    {
+                        Value = 9,
+                        Next = null
+                    }
+                }
+            };
+
+            var l2 = new Node
+            {
+                Value = 1,
+                Next = new Node
+                {
+                    Value = 6,
+                    Next = new Node
+                    {
+                        Value = 9,
+                        Next = new Node
+                        {
+                            Value = 2,
+                            Next = null
+                        }
+                    }
+                }
+            };
+
+            var result = _recursiveLinkedListSum.Solution(l1, l2);
+
+            int[] answer = {0, 6, 9, 3};
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Assert.AreEqual(answer[i], result.Value);
+                result = result.Next;
+            }
+        }
+    }
+}
---
+++ LeetCode/Medium/Recursion/SumOfTwoLinkedLists.cs
@@ -0,0 +1,72 @@
+﻿namespace LeetCode.Medium.Recursion
+{
+    //Definition of node
+
+    public class Node
+    {
+        public Node Next { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class SumOfTwoLinkedLists
+    {
+        public Node Solution(Node l1, Node l2)
+        {
+            return Sum(l1, l2, 0, null);
+        }
+
+        public Node Sum(Node l1, Node l2, int carry, Node result)
+        {
+            if (l1 == null && l2 == null)
+            {
+                if(carry > 0)
+                {
+                    return AppendElementToTheEnd(result, carry);
+                }
+                return result;
+            }
+
+            if (l1 == null || l2 == null)
+            {
+                var withValue = l1 ?? l2;
+                var sum = withValue.Value + carry;
+                int remainder =  sum % 10;
+
+                return Sum(null, withValue.Next, sum / 10, AppendElementToTheEnd(result, remainder));
+            }
+            else
+            {
+                var sum = l1.Value + l2.Value + carry;
+                int remainder = sum % 10;
+
+                return Sum(l1.Next, l2.Next, sum / 10 , AppendElementToTheEnd(result, remainder));
+            }
+        }
+
+        public Node AppendElementToTheEnd(Node l1, int value)
+        {
+            if (l1 == null)
+            {
+                return new Node
+                {
+                    Value = value
+                };
+            }
+
+            var head = l1;
+
+            while(head.Next != null)
+            {
+                head = head.Next;
+            }
+
+            head.Next = new Node
+            {
+                Value = value,
+                Next = null
+            };
+
+            return l1;
+        }
+    }
+}
