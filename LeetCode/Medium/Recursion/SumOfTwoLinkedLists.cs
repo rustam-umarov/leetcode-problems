c83e6e6a7e39296b5ca859aca43e1ead175a13b3
Rustam Umarov <rustam.umarov@yahoo.com>
2021-11-11 23:03:33 -0600

    comments to recursive sum of two linked lists: medium

--- LeetCode/Medium/Recursion/SumOfTwoLinkedLists.cs
+++ LeetCode/Medium/Recursion/SumOfTwoLinkedLists.cs
@@ -18,32 +18,45 @@
 
         public Node Sum(Node l1, Node l2, int carry, Node result)
         {
+            // base case to check if there's nothing else left
             if (l1 == null && l2 == null)
             {
+                // in cases when there's a carry
+                // just append it at the very end
                 if(carry > 0)
                 {
                     return AppendElementToTheEnd(result, carry);
                 }
+                // else return the result
                 return result;
             }
 
+            // checking if one of the nodes is null
             if (l1 == null || l2 == null)
             {
+                // figure out which node is null and
+                // calculate sum and remainder
                 var withValue = l1 ?? l2;
                 var sum = withValue.Value + carry;
                 int remainder =  sum % 10;
 
+                // recursively calculate sum for the next node,
+                // which will be the value of the node + carry of there's any
                 return Sum(null, withValue.Next, sum / 10, AppendElementToTheEnd(result, remainder));
             }
             else
             {
+                // case when two nodes have values
                 var sum = l1.Value + l2.Value + carry;
                 int remainder = sum % 10;
 
+                // recursively pass values of both nodes, carry and append
+                // new value at the end of the result
                 return Sum(l1.Next, l2.Next, sum / 10 , AppendElementToTheEnd(result, remainder));
             }
         }
 
+        // helper method to append value at the end of linked list
         public Node AppendElementToTheEnd(Node l1, int value)
         {
             if (l1 == null)
