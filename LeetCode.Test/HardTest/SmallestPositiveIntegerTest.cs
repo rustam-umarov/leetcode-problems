c9e84a25143b9d61d3913979adf0e99296c21f73
Rustam Umarov <rustam.umarov@yahoo.com>
2021-11-17 20:37:20 -0600

    Smallest positive int in non sorted array: hard

---
+++ LeetCode.Test/HardTest/SmallestPositiveIntegerTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Hard;
+
+namespace LeetCode.Test.HardTest
+{
+    [TestClass]
+    public class SmallestPositiveIntegerTest
+    {
+        private SmallestPositiveInteger _medianOfTwoSortedArrays;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _medianOfTwoSortedArrays = new SmallestPositiveInteger();
+        }
+
+        [TestMethod]
+        public void GivenArrayWithMissingNumberAtTheEnd()
+        {
+            var result = _medianOfTwoSortedArrays.Solution(new int[] { 1, 2, 0 });
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithNegativeNumber()
+        {
+            var result = _medianOfTwoSortedArrays.Solution(new int[] { 3, 4, -1, 1 });
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GivenArrayWithMissingNumberInTheMiddle()
+        {
+            var result = _medianOfTwoSortedArrays.Solution(new int[] { 7, 8, 9, 11, 12 });
+            Assert.AreEqual(1, result);
+        }
+
+
+        [TestMethod]
+        public void GivenTwoNumbers()
+        {
+            var result = _medianOfTwoSortedArrays.Solution(new int[] { 2, 1});
+            Assert.AreEqual(3, result);
+        }
+    }
+}
+
---
+++ LeetCode/Hard/SmallestPositiveInteger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace LeetCode.Hard
+{
+    public class SmallestPositiveInteger
+    {
+        public int Solution(int[] nums)
+        {
+            var set = new SortedSet<int>();
+
+            // add all items to set
+            Array.ForEach(nums, x => set.Add(x));
+
+            // smallest positive int is either any int
+            // starting with 1 up to nums.Length,
+            // otherwise max number from set + 1
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (!set.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            // no min int found in the beginning
+            // returning max + 1 from set
+            return set.Max + 1;
+        }
+    }
+}
