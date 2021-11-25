074dddaed226db90d1248b6bd2de13cde5691045
Rustam Umarov <rustam.umarov@yahoo.com>
2021-11-24 21:41:56 -0600

    medium of 2 sorted arrays: Hard

--- LeetCode.Test/HardTest/MedianOfTwoSortedArraysTest.cs
+++ LeetCode.Test/HardTest/MedianOfTwoSortedArraysTest.cs
@@ -14,21 +14,31 @@ namespace LeetCode.Test.HardTest
             _medianOfTwoSortedArrays = new MedianOfTwoSortedArrays();
         }
 
+        [TestMethod]
         public void GivenTwoSimpleSortedArrays()
         {
             var result = _medianOfTwoSortedArrays.Solution(new int[]{ 1, 2}, new int[]{ 3, 4 });
             Assert.AreEqual(2.5, result);
         }
 
+        [TestMethod]
         public void GivenUnequalArrays()
         {
             var result = _medianOfTwoSortedArrays.Solution(new int[] { 1, 3 }, new int[] { 2 });
             Assert.AreEqual(2, result);
         }
 
+        [TestMethod]
         public void GivenRepeatingNumbers()
         {
             var result = _medianOfTwoSortedArrays.Solution(new int[] { 1, 1 }, new int[] { 1, 2 });
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void GivenOneEmptyArray()
+        {
+            var result = _medianOfTwoSortedArrays.Solution(new int[] { }, new int[] { 1});
             Assert.AreEqual(1, result);
         }
     }
--- LeetCode/Hard/MedianOfTwoSortedArrays.cs
+++ LeetCode/Hard/MedianOfTwoSortedArrays.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LeetCode.Hard
@@ -7,9 +8,95 @@ namespace LeetCode.Hard
     {
         public double Solution(int[] nums1, int[] nums2)
         {
-            // TODO
-            // solve this asap
+            // get sum of length of 2 arrays
+            var length = nums1.Length + nums2.Length;
+            var result = new double[length / 2 + 1];
+            var isEven = length % 2 == 0;
+
+            // find the smallest
+            var largeArray = nums1.Length < nums2.Length ? nums2: nums1;
+            var smallArray = nums1.Length < nums2.Length ? Array.ConvertAll<int, double>(nums1, x => x) : Array.ConvertAll<int, double>(nums2, x => x);
+
+            if(nums1.Length == 0 || nums2.Length == 0)
+            {
+                if(nums1.Length == 0)
+                {
+                    result[0] = nums2[0];
+                }
+                else
+                {
+                    result[0] = nums1[0];
+                }
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if(i == 0)
+                {
+                    continue;
+                }
+
+                var searchResult = i == largeArray.Length ? BinarySearch(result[i - 1], smallArray)
+                    :  BinarySearch(largeArray[i], smallArray);
+
+                if(i > largeArray.Length - 1)
+                {
+                    result[i] = searchResult ?? 0;
+                }
+                else if (largeArray[i] <= searchResult)
+                {
+                    result[i] = largeArray[i];
+                }
+                else
+                {
+                    result[i] = searchResult ?? 0;
+                }
+
+
+                if (i == result.Length - 1)
+                {
+                    if (isEven)
+                    {
+                        return (result.Last() + result[result.Length - 2]) / 2;
+                    }
+                    else
+                    {
+                        return result.Last();
+                    }
+                }
+            }
             return 1;
+        }
+
+
+        private double? BinarySearch(double num, double[] nums)
+        {
+            int min = 0;
+            int max = nums.Length - 1;
+            double? temp = null;
+
+            while (min <= max)
+            {
+                if (max - min == 1 || max - min == 0)
+                {
+                    return nums[min];
+                }
+
+                int mid = (min + max) / 2;
+                if (num == nums[mid])
+                {
+                    temp = nums[++mid];
+                }
+                else if (num < nums[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return temp;
         }
     }
 }
